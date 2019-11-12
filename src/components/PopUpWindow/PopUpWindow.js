b8fbb5ade6fe7ead1a56e304a965e14b2ca7106c
XiaoyanZhang0999 <yannis.zxy@gmail.com>
2019-11-12 12:27:42 -0500

    react.component

--- src/components/PopUpWindow/PopUpWindow.js
+++ src/components/PopUpWindow/PopUpWindow.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
-export default class PopUpWindow extends Component {
+export default class PopUpWindow extends React.Component {
   constructor(props) {
     super(props);
     this.state = { modal: false,name: ''};
